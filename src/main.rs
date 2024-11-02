1ee9fbaff869e2d16cf9a9f8e8b62e9f5ec44680
Daiki48 <daiki48.engineer@gmail.com>
2024-11-02 18:48:10 +0900

    :white_check_mark: Added test for fetching plugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{command, Parser};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use std::collections::VecDeque;
 
@@ -13,7 +13,7 @@ struct Args {
     list: bool,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Package {
     name: String,
 }
@@ -46,4 +46,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use httpmock::MockServer;
+    use httpmock::Method::GET;
+
+    #[tokio::test]
+    async fn test_fetch_plugins() -> Result<(), reqwest::Error> {
+        let server = MockServer::start();
+
+        let plugins = vec![
+            Package { name: "plugin1".to_string() },
+            Package { name: "plugin2".to_string() },
+        ];
+
+        let plugins_json = serde_json::to_string(&plugins).unwrap();
+        
+        let mock = server.mock(|when, then| {
+            when.method(GET)
+                .path("/v1/packages")
+                .query_param("page", "0")
+                .query_param("sort", "majority");
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(plugins_json);
+        });
+
+        let url = format!("{}/v1/packages?page=0&sort=majority", server.base_url());
+        let response : Vec<Package> = reqwest::get(&url).await?.json().await?;
+
+        assert_eq!(response.len(), 2);
+        assert_eq!(response[0].name, "plugin1");
+        assert_eq!(response[1].name, "plugin2");
+
+        mock.assert();
+        Ok(())
+    }
 }
